717cc98078d4dfaa3033f54d95c27931bbc8dce1
nyh <2830481694@qq.com>
2025-02-13 13:34:57 +0800

    feature: using winnow parse josn data

--- src/json.rs
+++ src/json.rs
@@ -1,66 +1,69 @@
+use anyhow::{anyhow, Result};
 use std::collections::HashMap;
-
-use anyhow::Result;
 use winnow::{
-    ascii::{digit1, float},
-    combinator::{alt, delimited, opt, separated, separated_pair},
+    ascii::{digit1, multispace0},
+    combinator::{alt, delimited, opt, separated, separated_pair, trace},
+    error::{ContextError, ErrMode, ParserError},
+    prelude::*,
+    stream::{AsChar, Stream, StreamIsPartial},
     token::take_until,
-    Parser,
 };
 
+#[derive(Debug, Clone, PartialEq)]
+enum Num {
+    Int(i64),
+    Float(f64),
+}
+
 #[allow(unused)]
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 enum JsonValue {
     Null,
     Bool(bool),
-    Int(i64),
-    Number(f64),
+    Number(Num),
     String(String),
     Array(Vec<JsonValue>),
     Object(HashMap<String, JsonValue>),
 }
 
-#[allow(unused)]
-#[derive(Debug)]
-struct User {
-    name: String,
-    age: u32,
-    is_student: bool,
-    marks: Vec<u32>,
-    address: Address,
-}
-
-#[allow(unused)]
-#[derive(Debug)]
-struct Address {
-    city: String,
-    zip: u32,
-}
-
 fn main() -> Result<()> {
-    let s = r#"
-    {
-      "name": "John Doe",
-      "age": 30,
-      "is_student": false,
-      "marks": [90, -80, 85],
-      address: {
-        "city": "London",
-        "zip": 10001
-      }
+    let s = r#"{
+        "name": "John Doe",
+        "age": 30,
+        "is_student": false,
+        "marks": [90.0, -80.0, 85.1],
+        "address": {
+            "city": "New York",
+            "zip": 10001
+        }
     }"#;
 
-    parse_json(s).unwrap();
-    // println!("{:#?}", v);
-
+    let input = &mut (&*s);
+    let v = parse_json(input)?;
+    println!("{:#?}", v);
     Ok(())
 }
 
-fn parse_json(input: &str) -> winnow::Result<()> {
-    let mut input = input;
-    let user = parse_object.parse_next(&mut input)?;
-    println!("{:?}", user);
-    Ok(())
+fn parse_json(input: &str) -> Result<JsonValue> {
+    let input = &mut (&*input);
+    parse_value(input).map_err(|e: ContextError| anyhow!("Failed to parse JSON: {:?}", e))
+}
+
+pub fn sep_with_space<Input, Output, Error, ParseNext>(
+    mut parser: ParseNext,
+) -> impl Parser<Input, (), Error>
+where
+    Input: Stream + StreamIsPartial,
+    <Input as Stream>::Token: AsChar + Clone,
+    Error: ParserError<Input>,
+    ParseNext: Parser<Input, Output, Error>,
+{
+    trace("sep_with_space", move |input: &mut Input| {
+        let _ = multispace0.parse_next(input)?;
+        parser.parse_next(input)?;
+        multispace0.parse_next(input)?;
+        Ok(())
+    })
 }
 
 fn parse_null(input: &mut &str) -> winnow::Result<()> {
@@ -71,43 +74,180 @@ fn parse_bool(input: &mut &str) -> winnow::Result<bool> {
     alt(("true", "false")).parse_to().parse_next(input)
 }
 
-fn parse_int(input: &mut &str) -> winnow::Result<i64> {
-    let sign = opt('-').map(|s| s.is_some()).parse_next(input)?;
+// FIXME: num parse doesn't work with scientific notation, fix it
+fn parse_num(input: &mut &str) -> winnow::Result<Num> {
+    // process the sign
+    let sign = opt("-").map(|s| s.is_some()).parse_next(input)?;
     let num = digit1.parse_to::<i64>().parse_next(input)?;
-    Ok(if sign { -num } else { num })
+    let ret: Result<(), ErrMode<ContextError>> = ".".value(()).parse_next(input);
+    if ret.is_ok() {
+        let frac = digit1.parse_to::<String>().parse_next(input)?;
+        let v = format!("{}.{}", num, frac)
+            .parse::<f64>()
+            .expect("string should be a valid float");
+        Ok(if sign {
+            Num::Float(-v as _)
+        } else {
+            Num::Float(v as _)
+        })
+    } else {
+        Ok(if sign { Num::Int(-num) } else { Num::Int(num) })
+    }
 }
 
-fn parse_number(input: &mut &str) -> winnow::Result<f64> {
-    float.parse_next(input)
-}
-
+// json allows quoted strings to have escaped characters, we won't handle that here
 fn parse_string(input: &mut &str) -> winnow::Result<String> {
     let ret = delimited('"', take_until(0.., '"'), '"').parse_next(input)?;
     Ok(ret.to_string())
 }
 
 fn parse_array(input: &mut &str) -> winnow::Result<Vec<JsonValue>> {
-    let parse_value = separated(0.., parse_value, ',');
-    let ret = delimited('[', parse_value, ']').parse_next(input)?;
-    Ok(ret)
+    let sep1 = sep_with_space('[');
+    let sep2 = sep_with_space(']');
+    let sep_comma = sep_with_space(',');
+    let parse_values = separated(0.., parse_value, sep_comma);
+    delimited(sep1, parse_values, sep2).parse_next(input)
 }
 
 fn parse_object(input: &mut &str) -> winnow::Result<HashMap<String, JsonValue>> {
-    let parser_kv_pair = separated_pair(parse_string, ":", parse_value);
-    let parse_kv = separated(1.., parser_kv_pair, ',');
-    let ret = delimited('{', parse_kv, '}').parse_next(input)?;
-    Ok(ret)
+    let sep1 = sep_with_space('{');
+    let sep2 = sep_with_space('}');
+    let sep_comma = sep_with_space(',');
+    let sep_colon = sep_with_space(':');
+
+    let parse_kv_pair = separated_pair(parse_string, sep_colon, parse_value);
+    let parse_kv = separated(0.., parse_kv_pair, sep_comma);
+    delimited(sep1, parse_kv, sep2).parse_next(input)
 }
 
 fn parse_value(input: &mut &str) -> winnow::Result<JsonValue> {
     alt((
         parse_null.value(JsonValue::Null),
         parse_bool.map(JsonValue::Bool),
-        parse_int.map(JsonValue::Int),
-        parse_number.map(JsonValue::Number),
+        parse_num.map(JsonValue::Number),
         parse_string.map(JsonValue::String),
         parse_array.map(JsonValue::Array),
         parse_object.map(JsonValue::Object),
     ))
     .parse_next(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_null() -> winnow::Result<(), ContextError> {
+        let input = "null";
+        parse_null(&mut (&*input))?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_bool() -> winnow::Result<(), ContextError> {
+        let input = "true";
+        let result = parse_bool(&mut (&*input))?;
+        assert!(result);
+
+        let input = "false";
+        let result = parse_bool(&mut (&*input))?;
+        assert!(!result);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_num() -> winnow::Result<(), ContextError> {
+        let input = "123";
+        let result = parse_num(&mut (&*input))?;
+        assert_eq!(result, Num::Int(123));
+
+        let input = "-123";
+        let result = parse_num(&mut (&*input))?;
+        assert_eq!(result, Num::Int(-123));
+
+        let input = "123.456";
+        let result = parse_num(&mut (&*input))?;
+        assert_eq!(result, Num::Float(123.456));
+
+        let input = "-123.456";
+        let result = parse_num(&mut (&*input))?;
+        assert_eq!(result, Num::Float(-123.456));
+
+        // FIXME: doesn't work it the digital starts with 0
+        let input = "123.0001";
+        let result = parse_num(&mut (&*input))?;
+        assert_eq!(result, Num::Float(123.0001));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_string() -> winnow::Result<(), ContextError> {
+        let input = r#""hello""#;
+        let result = parse_string(&mut (&*input))?;
+        assert_eq!(result, "hello");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_array() -> winnow::Result<(), ContextError> {
+        let input = r#"[1, 2, 3]"#;
+        let result = parse_array(&mut (&*input))?;
+
+        assert_eq!(
+            result,
+            vec![
+                JsonValue::Number(Num::Int(1)),
+                JsonValue::Number(Num::Int(2)),
+                JsonValue::Number(Num::Int(3))
+            ]
+        );
+
+        let input = r#"["a", "b", "c"]"#;
+        let result = parse_array(&mut (&*input))?;
+        assert_eq!(
+            result,
+            vec![
+                JsonValue::String("a".to_string()),
+                JsonValue::String("b".to_string()),
+                JsonValue::String("c".to_string())
+            ]
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_object() -> winnow::Result<(), ContextError> {
+        let input = r#"{"a": 1, "b": 2}"#;
+        let result = parse_object(&mut (&*input))?;
+        let mut expected = HashMap::new();
+        expected.insert("a".to_string(), JsonValue::Number(Num::Int(1)));
+        expected.insert("b".to_string(), JsonValue::Number(Num::Int(2)));
+        assert_eq!(result, expected);
+
+        let input = r#"{"a": 1, "b": [1, 2, 3]}"#;
+        let result = parse_object(&mut (&*input))?;
+        let mut expected = HashMap::new();
+        expected.insert("a".to_string(), JsonValue::Number(Num::Int(1)));
+        expected.insert(
+            "b".to_string(),
+            JsonValue::Array(vec![
+                JsonValue::Number(Num::Int(1)),
+                JsonValue::Number(Num::Int(2)),
+                JsonValue::Number(Num::Int(3)),
+            ]),
+        );
+        assert_eq!(result, expected);
+
+        // FIXME: doesn't work with empty object
+        let input = r#"{}"#;
+        let result = parse_object(&mut (&*input))?;
+        let expected = HashMap::new();
+        assert_eq!(result, expected);
+
+        Ok(())
+    }
 }
